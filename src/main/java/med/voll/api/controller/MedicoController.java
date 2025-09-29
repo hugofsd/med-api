abf62c8f0b888fc0fe28d0a4cc30396698a73c39
hugofsd <hugo619@outlook.com>
2025-09-29 19:51:18 -0300

    Consultar médicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid; // Anotação para acionar a validação de um objeto aninhado.
 import med.voll.api.medico.DadosCadastroMedico; // Importa o DTO para cadastro de médicos.
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico; // Importa a entidade Medico.
 import med.voll.api.medico.MedicoRepository; // Importa o repositório para a entidade Medico.
 import org.springframework.beans.factory.annotation.Autowired; // Anotação para injeção de dependência automática.
 import org.springframework.transaction.annotation.Transactional; // Anotação para demarcar um método ou classe como transacional.
-import org.springframework.web.bind.annotation.PostMapping; // Anotação para mapear requisições HTTP POST.
-import org.springframework.web.bind.annotation.RequestBody; // Anotação que indica que um parâmetro de método deve ser vinculado ao corpo da requisição web.
-import org.springframework.web.bind.annotation.RequestMapping; // Anotação para mapear requisições web.
-import org.springframework.web.bind.annotation.RestController; // Anotação para criar um controlador de API REST.
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,5 +22,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
