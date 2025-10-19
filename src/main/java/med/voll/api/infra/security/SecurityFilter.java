be85300573adb8c2a5b3b927a4dd3c8334cd1476
hugofsd <hugo619@outlook.com>
2025-10-18 21:25:34 -0300

    Criando o filter de segurança

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,26 @@
+package med.voll.api.infra.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component // componente generico
+public class SecurityFilter extends OncePerRequestFilter {
+
+    // implmentes de OncePerRequestFilter
+    // garante uma validação por request
+    // Responsavel por validar token. Intercepta todas as requisições.
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //doFilter garante que vamos chamar a proxima requisição.
+        // quer interromper não vamos chamar o doFilter
+        filterChain.doFilter(request, response);
+    }
+}
