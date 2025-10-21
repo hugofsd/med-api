587f4691201812d58ed486959a3a0b8feef0ca32
hugofsd <hugo619@outlook.com>
2025-10-21 10:58:04 -0300

    Validando o token recebido

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,29 +14,33 @@ import java.io.IOException;
 @Component // componente generico
 public class SecurityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private TokenService tokenService;
+
     // implmentes de OncePerRequestFilter
     // garante uma validação por request
     // Responsavel por validar token. Intercepta todas as requisições.
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        //recuperar token
         var tokenJWT = recuperarToken(request);
 
-        System.out.println("TOKEN: " + tokenJWT);
+        var subject = tokenService.getSubjectValidationToken(tokenJWT);
+
+
 
         //doFilter garante que vamos chamar a proxima requisição.
         // quer interromper não vamos chamar o doFilter
         filterChain.doFilter(request, response);
     }
 
-    private Object recuperarToken(HttpServletRequest request) {
+    private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
 
         if(authorizationHeader == null){
             throw new RuntimeException("Token JWT não enviado no cabeçalho Authorizarion");
         }
                                             // tirando o Bearer por aspas vaizas
-        return  authorizationHeader.replace("Bearer", "");
+        return authorizationHeader.replace("Bearer ", "").trim();
     }
 }
